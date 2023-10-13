fff933792f525c05d9de1236c858ef193afcb1c5
Glowingstone124 <80297629+glowingstone124@users.noreply.github.com>
2023-10-13 22:56:08 +0800

    Sync

--- src/main/java/org/glowingstone/ClipboardDataPoster.java
+++ src/main/java/org/glowingstone/ClipboardDataPoster.java
@@ -7,14 +7,18 @@ import org.json.JSONObject;
 import java.sql.Timestamp;
 
 public class ClipboardDataPoster {
-    public static boolean Post(String content) throws Exception{
+    public static boolean Post(String content){
         long currentTimeMillis = System.currentTimeMillis();
         Timestamp timestamp = new Timestamp(currentTimeMillis);
         JSONObject clipboardObj = new JSONObject();
         clipboardObj.put("timestamp", timestamp.toString());
         clipboardObj.put("content", content);
         clipboardObj.put("device", Device.getDevice());
-        Request.sendPostRequest(clipboardObj.toString(), Device.getPostApi());
-        return true;
+        try {
+            Request.sendPostRequest(clipboardObj.toString(), Device.getPostApi());
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
